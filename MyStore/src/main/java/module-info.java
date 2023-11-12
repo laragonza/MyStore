da1bc5b2d23dd9a0a05b6add33d353b5329e5a21
lari_gnz <laragonza1294@gmail.com>
2023-11-12 18:25:21 +0100

    module-info.java

--- MyStore/src/main/java/module-info.java
+++ MyStore/src/main/java/module-info.java
@@ -1,12 +1,15 @@
 module com.example.mystore {
+
+    // Requiere las bibliotecas necesarias para JavaFX
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
     requires com.dlsc.formsfx;
     requires javafx.media;
 
-
-
+    // Abre el paquete com.example.mystore para que JavaFX pueda acceder a él
     opens com.example.mystore to javafx.fxml;
+
+    // Exporta el paquete com.example.mystore, lo que permite que otras partes del código puedan utilizarlo
     exports com.example.mystore;
 }
