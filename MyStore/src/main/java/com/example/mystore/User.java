dfc191bd69a5302214507003b18ce89d25685443
lari_gnz <laragonza1294@gmail.com>
2023-11-12 18:24:54 +0100

    User.java

--- MyStore/src/main/java/com/example/mystore/User.java
+++ MyStore/src/main/java/com/example/mystore/User.java
@@ -1,16 +1,22 @@
 package com.example.mystore;
 
+// Clase que representa a un usuario en la aplicación
 public class User {
+    
+    // Atributo que almacena el nombre de usuario
     private String username;
 
+    // Constructor para inicializar un objeto User con un nombre de usuario proporcionado
     public User(String username) {
         this.username = username;
     }
 
+    // Getter para obtener el nombre de usuario
     public String getUsername() {
         return username;
     }
 
+    // Setter para establecer el nombre de usuario
     public void setUsername(String username) {
         this.username = username;
     }
